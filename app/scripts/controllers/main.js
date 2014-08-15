312f26a3f236ffa92702eef52f0c1a204085c191
Andrew <13761224431@163.com>
2014-08-15 19:03:14 +0800

    Initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kineticCopyApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the kineticCopyApp
+ */
+angular.module('kineticCopyApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
